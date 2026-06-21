4a65f97fcc32b2ab561cbd1d472e9efddff5c4e6
Zawe Yan Naing <zaweyannaing121.phys@gmail.com>
2026-06-21 20:43:47 +0630

    fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,8 +10,8 @@ export default function Hero() {
           initial={{ scale: 0 }}
           animate={{ scale: 1 }}
           transition={{ duration: 0.5 }}
-          src="/images/hhp.jpg"
-          alt={profile}
+          src={profile}
+          alt="Profile"
           className="w-32 h-32 md:w-40 md:h-40 rounded-full mx-auto mb-8 object-cover shadow-xl"
         />
         <motion.h1
